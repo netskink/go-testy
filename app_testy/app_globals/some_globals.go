7553ccfae593c32751e3de5e461cd3ee68b68933
John F. Davis <davisjf@gmail.com>
2022-02-15 06:31:48 -0500

    wip

---
+++ app_testy/app_globals/some_globals.go
@@ -0,0 +1,6 @@
+package app_globals
+
+var (
+	Var1 string = "yo"
+	Var2 string = "ho"
+)
---
+++ app_testy/app_main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello from go")
+
+	fmt.Println("var1 is ", Var1)
+	fmt.Println("var2 is ", Var2)
+}
